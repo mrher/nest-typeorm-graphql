f63c4bd659f5bae6f2fcb9de9b69f3b9d7cbd9d3
Sergei G <mrhersergik@gmail.com>
2024-09-22 14:58:19 +0700

    establish database

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,13 +6,14 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
+        host: '127.0.0.1',
         port: 3306,
         username: 'root',
-        password: 'root',
-        database: 'test',
+        password: 'gfhfnhegth51',
+        database: 'nest_test_db',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
+        migrations: ['src/migrations/*{.ts,.js}'],
+        synchronize: false,
       });
 
       return dataSource.initialize();
